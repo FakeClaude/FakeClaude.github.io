23f77317dd871a02cfae4130ee1de7d74d9a1bb8
FakeClaude <fakechina2028@gmail.com>
2026-07-21 03:33:54 +0800

    + theme.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@
 import { h, render } from "preact";
 import App from "./index.jsx";
 import { i18nPromise } from "./utils/i18n.js";
+import { initTheme } from "./utils/theme.js";
 
-// 挂载深色主题(对应 main.css 里的 :root[data-theme="dark"] 那套变量)
-document.documentElement.setAttribute("data-theme", "dark");
+initTheme();
 
 i18nPromise.then(() => {
   render(<App />, document.getElementById("app"));
